8017903b814daef173dd34d4e1e57e9789bf153a
DeProdigy <alex.hint.ah@gmail.com>
2014-12-20 13:37:44 -0500

    Set up search controller and a simple one page search functionality.

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('.search-songs').on('submit', function(e) {
+    e.preventDefault();
+    getSoundsCloudSongs();
+  });
+});
+
+function getSoundsCloudSongs() {
+  $.ajax({
+    type: "POST",
+    url: '/search',
+    data: {
+      'search': {
+        'query': $('#user-input').val()
+      }
+    },
+    success: function(data) {
+      // debugger;
+    }
+  });
+}
---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+  def create
+    search_query
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:query)
+  end
+
+  def search_query
+    search_params[:query]
+  end
+end
