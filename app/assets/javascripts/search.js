53b7c9600ace77495a69823c4705f331e0592c75
ByteDown <igor.yuzovitskiy@gmail.com>
2014-12-21 16:34:56 -0500

    added displaySearchResults to display first 5 results from soundcloud API and created a template to display list of 5 songs

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -15,7 +15,50 @@ function getSoundsCloudSongs() {
       }
     },
     success: function(data) {
-      console.log(data);
+      displaySearchResults(data);
     }
   });
 }
+
+function displaySearchResults(data) {
+
+  $.each(data.tracks, function(index, value) {
+    debugger;
+    balls(value);
+    // var playList =
+    // $(
+    //   '''
+    //     <div class="tracks" data-user-name=' value.user_name ' data-title=' value.title ' data-stream-url=' value.stream_url '>
+    //       <h4>' value.user_name '</h4>
+    //       <h6>' value.title '</h6>
+    //       <audio src=' value.stream_url ' controls loop></audio>
+    //     </div>
+    //   '''
+    // );
+  });
+
+  // body.append(playList);
+
+  // for(var i = 0; i < data.tracks.length; i++) {
+  //   var body = $('body');
+  //   var userName = "<h4>" + data.tracks[i].user_name + "</h4>";
+  //   var title = "<h6>" + data.tracks[i].title + "</h6>";
+  //   var streamUrl = "<audio src=" + data.tracks[i].stream_url + " controls loop></audio>";
+  //   var track = $("<div class='tracks' data-name='NAME'></div>");
+  //   body.append(track);
+  //   track.append(userName, title, streamUrl);
+  //   track.on("click", function() {
+  //     debugger;
+  //     selectTrack(this.data());
+  //   });
+  // }
+}
+
+function balls(huy) {
+  debugger
+  console.log('HERE')
+}
+
+// function selectTrack(track) {
+//   console.log(track);
+// }
---
+++ app/assets/javascripts/search_template.coffee
@@ -0,0 +1,11 @@
+
+console.log('CONNECT')
+@tracksTemplate = (track) ->
+  # playList =
+  # '''
+  #   <div class="tracks" data-user-name=' track.user_name ' data-title=' track.title ' data-stream-url=' track.stream_url '>
+  #     <h4>' track.user_name '</h4>
+  #     <h6>' track.title '</h6>
+  #     <audio src=' track.stream_url ' controls loop></audio>
+  #   </div>
+  # '''
