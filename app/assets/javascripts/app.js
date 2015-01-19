102b33ddc6ea64ccb8e40101fe5529bb8cc281b9
DeProdigy <alex.hint.ah@gmail.com>
2015-01-19 15:19:30 -0500

    Set up the custom JS framework basics. [Finishes #86376442]

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,8 @@
+// Define FlowList object + aliases
+var FL    = {
+              models: {
+                songs: []
+              }
+            },
+  models  = FL.models,
+  songs   = FL.models.songs;
