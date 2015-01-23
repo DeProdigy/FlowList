b015549c81b05ae593cbcdf9826a59b622d727b5
ByteDown <igor.yuzovitskiy@gmail.com>
2015-01-22 19:15:43 -0500

    Pull from master to align local repo with github repo.

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222223725) do
+ActiveRecord::Schema.define(version: 20150110173755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "songs", force: true do |t|
-    t.integer  "soundcloud_id"
-    t.string   "title"
-    t.string   "user_name"
-    t.string   "stream_url"
-    t.integer  "duration"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
