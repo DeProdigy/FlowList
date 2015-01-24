3bfd15c8262df9627c57745302e07845cc2c800c
DeProdigy <alex.hint.ah@gmail.com>
2015-01-24 14:32:15 -0500

    Basic CSS framework setup.

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 if Rails.env.test?
-      require 'simplecov'
-      SimpleCov.start
-  end
+  require 'simplecov'
+  SimpleCov.start
+end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +22,8 @@ module FlowList
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
   end
 end
